7b9ca73af1322fd07fedbb7d8c814b36581dd064
DTIV <dan_troyer@hotmail.com>
2021-07-22 14:39:03 -0400

    reference counting

---
+++ varandmem.py
@@ -0,0 +1,39 @@
+'''
+this is a follow through of the Python Deep Dive course by Fred Baptiste on Udemy
+hands down the best python course on udemy by far 
+'''
+import sys
+import ctypes
+'''
+Variables are references to memory addresses
+ -find out the memory address of a variable using id()
+ -id() returns a base-10 number
+ -convert the base-10 to a hexidecimal using hex()
+
+'''
+a = [1,2,3]
+
+print("variable memory address in base-10 and hexidecimal:")
+print("base-10:", id(a))
+print("hexidecimal:",hex(id(a)),"\n")
+
+
+'''
+Two variables can reference the same memory address
+'''
+new_var = a
+print("two variables referencing the same memory address:")
+print(id(new_var))
+print(id(a),"\n")
+
+'''
+Get the count of references at a memory address using getrefcount
+ -getrefcount adds another reference count to the memory address
+ -ctypes does not add another reference count
+'''
+
+def ref_count(address: int):
+    return ctypes.c_long.from_address(address).value
+
+print("getrefcount() adding another reference count:",sys.getrefcount(a))
+print('true reference count using ctypes:',ref_count(id(a)),"\n")
